8260db74dccfddcc6f7b8318e9b96472ff93b5f3
Janis <jcostadelli@gmail.com>
2021-05-19 16:44:06 -0300

    continuação dos exercícios da aula 42

---
+++ semana15/Aula-42/exercicio-1/index.js
@@ -0,0 +1,3 @@
+"use strict";
+var minhaString = 58; /*Se adicionar um número na variável do tipo string, ocorre um erro que informa que não se pode adicionar numeros em variáveis do tipo string */
+var meuNumero = 39;
--- semana15/Aula-42/exercicio-1/index.ts
+++ semana15/Aula-42/exercicio-1/index.ts
@@ -1,3 +1,32 @@
-const minhaString: string = 58 /*Se adicionar um número na variável do tipo string, ocorre um erro que informa que não se pode adicionar numeros em variáveis do tipo string */ 
+/*Exercício 1: a) Se adicionar um número na variável do tipo string, ocorre um erro que informa
+que não se pode adicionar numeros em variáveis do tipo string (tirei o numero porque tava me dando nervoso de ver o erro) */ 
+const minhaString: string = "um numero" 
 
-const meuNumero: number = 39
+/*Exercício 1: b) */
+const meuNumero: number | string = 39
+
+/*Exercício 1: c) */
+
+type person = {
+    name: string,
+    age: number,
+    favoriteColor: string
+}
+
+const janis: person = {
+    name: "Janis Costadelli",
+    age: 18,
+    favoriteColor: "Azul"
+}
+
+const nicolas: person = {
+    name: "Nicolas Alexandre",
+    age: 15,
+    favoriteColor: "Preto"
+}
+
+const emily: person = {
+    name: "Emily Rosi",
+    age: 7,
+    favoriteColor: "Rosa"
+}
